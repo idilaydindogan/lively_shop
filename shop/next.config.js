f309c1017cdeab5de6239d4962a7dacbbfb610ee
Idil Dogan <idilaydindogan@gmail.com>
2023-03-22 17:37:31 +0300

    Mongodb added

--- shop/next.config.js
+++ shop/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+	},
+	env: {
+		DB_URI: "mongodb://localhost:27017/buyitnow",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
